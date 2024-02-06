1d983458610d70e1595933f9dee067654f5e9aeb
Deyán F. Guacarán Sabogal <deyan.fgs904@gmail.com>
2024-02-06 16:21:29 +0000

    wip: fixing vite error

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'cypress';
+import plugins from './cypress/plugins';
 
 export default defineConfig({
   e2e: {
-    // baseUrl: 'http://localhost:5173',
-    experimentalStudio: true,
+    baseUrl: 'http://localhost:3000',
+    specPattern: '**/*.spec.{js,ts}',
+    setupNodeEvents(on, config) {
+      return plugins(on, config);
+    },
   },
 });
